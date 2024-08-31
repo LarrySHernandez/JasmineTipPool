19727fb2aae23572c0b5de49d94550536b2283fd
Larry <larryh2612@gmail.com>
2024-08-30 22:55:34 -0400

    version 1.0

--- servers.test.js
+++ servers.test.js
@@ -21,7 +21,7 @@ describe("Servers test (with setup and tear-down)", function() {
 
 
 
-describe('testing function for submitServerInfo', function(){
+describe('testing functionality for submitServerInfo', function(){
   beforeEach(function(){
     serverNameInput.value = 'Jacob'
     submitServerInfo();
@@ -30,13 +30,46 @@ describe('testing function for submitServerInfo', function(){
     serverNameInput.value = 'Jose'
     submitServerInfo();
   });
-  it('should store the name inputed', function(){
-      expect();
+
+  it('should have only unique server Id for each server', function(){
+      let values = Object.keys(allServers);
+      let testResult = values.some((value1)=>{
+        let valuesCopy = values;
+        delete valuesCopy.splice(valuesCopy.indexOf(value1), 1)
+        return valuesCopy.some((value2)=> value1 !== value2)});
+      expect(testResult).toBe(true);
   });
-  it('should have only unique server Id', function(){
-      expect();
-  });
+
   afterEach(function(){
     allServers = {};
+    serverTbody.innerHTML = '';
+  });
+});
+
+
+describe('Testing functionality of updateServerTable', function(){
+  beforeEach(function(){
+    serverNameInput.value = 'Jacob'
+    submitServerInfo();
+    serverNameInput.value = 'Alex'
+    submitServerInfo();
+    serverNameInput.value = 'Jose'
+    submitServerInfo();
+    serverNameInput.value = 'Valerie'
+    submitServerInfo();
+  });
+
+    it('should have the same amount of tr as the amount of servers in allServers object', function(){
+      let values = Object.keys(allServers);
+      let allTr = Array.from(document.getElementsByTagName('tr'));
+
+      let allTrFiltered = allTr.slice(2, allTr.length - 2);
+      expect(allTrFiltered.length).toBe(values.length);
+    });
+
+
+  afterEach(function(){
+    allServers = {};
+    serverTbody.innerHTML = ''; 
   });
 });
